0f44a5d381237d3208a27bd6ba932f3eb525f8c9
torlunjen <tje012@uib.no>
2020-02-06 09:19:59 +0100

    Add methods to CellGrid to remove compile error

--- src/main/java/inf101/v20/lab3/cellular/datastructure/CellGrid.java
+++ src/main/java/inf101/v20/lab3/cellular/datastructure/CellGrid.java
@@ -1,5 +1,7 @@
 package inf101.v20.lab3.cellular.datastructure;
 
+import inf101.v20.lab3.cellular.CellState;
+
 /**
  * 
  * A Grid contains a set of {@link inf101.v20.lab3.cellular.CellState CellStates}
@@ -8,4 +10,28 @@ package inf101.v20.lab3.cellular.datastructure;
  */
 public class CellGrid implements IGrid {
 
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public void set(int x, int y, CellState element) {
+
+    }
+
+    @Override
+    public CellState get(int x, int y) {
+        return null;
+    }
+
+    @Override
+    public IGrid copy() {
+        return null;
+    }
 }
