650ec41c389789ffb54f1b95f423c6eaac95a3c4
torlunjen <tje012@uib.no>
2020-02-06 12:03:44 +0100

    All tests green

--- src/main/java/inf101/v20/lab3/cellular/datastructure/CellGrid.java
+++ src/main/java/inf101/v20/lab3/cellular/datastructure/CellGrid.java
@@ -2,36 +2,64 @@ package inf101.v20.lab3.cellular.datastructure;
 
 import inf101.v20.lab3.cellular.CellState;
 
+import java.util.ArrayList;
+
 /**
  * 
  * A Grid contains a set of {@link inf101.v20.lab3.cellular.CellState CellStates}
  * 
- * @author <YOUR NAME HERE> - <EMAIL>
+ * @author <Torbjørn Lunde Jensen> - <tje012@uib.no>
  */
 public class CellGrid implements IGrid {
 
+    private final int width;
+    private final int height;
+    ArrayList<CellState> cellStateList = new ArrayList<>();
+
+    public CellGrid(int width, int height, CellState state) {
+        this.width = width;
+        this.height = height;
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                cellStateList.add(state);
+            }
+        }
+    }
+
     @Override
     public int getHeight() {
-        return 0;
+        return height;
     }
 
     @Override
     public int getWidth() {
-        return 0;
+        return width;
     }
 
     @Override
     public void set(int x, int y, CellState element) {
-
+        if (x <= width && x >= 0 && y <= height && y >= 0) {
+            cellStateList.set(x * height + y, element);
+        }
+        else throw new IndexOutOfBoundsException();
     }
 
     @Override
     public CellState get(int x, int y) {
-        return null;
+        if(x <= width && x >= 0 && y <= height && y >= 0) {
+            return cellStateList.get(x * height + y);
+        }
+        else throw new IndexOutOfBoundsException();
     }
 
     @Override
     public IGrid copy() {
-        return null;
+        CellGrid newGrid = new CellGrid(this.getWidth(), this.getHeight(), CellState.DEAD);
+        for(int x = 0; x < this.getWidth(); x++) {
+            for(int y = 0; y < this.getHeight(); y++) {
+                newGrid.set(x, y, this.get(x, y));
+            }
+        }
+        return newGrid;
     }
 }
